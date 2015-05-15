d82742ab4a9755174e9689dd990bfb9b90180332
Jan Krems <jan.krems@groupon.com>
2015-05-15 08:32:57 -0700

    Precompile to have decent startup time

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import { createServer } from 'http';
+
+import createApp from 'quinn';
+import createRouter from 'wegweiser';
+import cass from 'cass'
+
+import todoRoutes from './todos/routes';
+
+const router = createRouter(...todoRoutes);
+
+function errorLogger(handler) {
+  return async req => {
+    try {
+      return await handler(req);
+    } catch (err) {
+      console.error(err.stack);
+      return Promise.reject(err);
+    }
+  };
+}
+
+const app = createApp(cass(errorLogger(router)));
+const server = createServer(app);
+
+server.listen(8000, function() {
+  console.log('Listening.');
+});
