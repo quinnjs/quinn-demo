b3c1f3b6c0cda0b2d6f06a6a30881bcad3bf6085
Jan Krems <jan.krems@groupon.com>
2015-05-15 08:19:03 -0700

    Using the latest libraries

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import { createServer } from 'http';
 
 import createApp from 'quinn';
 import createRouter from 'wegweiser';
+import cass from 'cass'
 
 import todoRoutes from './src/todos/routes';
 
@@ -18,7 +19,7 @@ function errorLogger(handler) {
   };
 }
 
-const app = createApp(errorLogger(router));
+const app = createApp(cass(errorLogger(router)));
 const server = createServer(app);
 
 server.listen(8000, function() {
--- src/todos/routes.js
+++ src/todos/routes.js
@@ -1,24 +1,10 @@
-import { json } from 'quinn/respond';
 import { GET, POST, PUT } from 'wegweiser';
-import __getRawBody from 'raw-body';
-import { promisify } from 'Bluebird';
-import { mapKeys, snakeCase } from 'lodash';
+import parsedBody from 'parsed-body/json';
+import { mapKeys, snakeCase, pick, isEmpty, filter } from 'lodash';
 
 import db from '../db';
 
 import Todo from './model';
-
-const getRawBody = promisify(__getRawBody);
-
-// TODO: turn this into a proper module
-async function readBody(req) {
-  const raw = await getRawBody(req, {
-    length: req.headers['content-length'],
-    limit: '1mb',
-    encoding: 'utf8'
-  });
-  return JSON.parse(raw);
-}
 
 function toDbSelector(property) {
   const column = snakeCase(property);
@@ -27,7 +13,7 @@ function toDbSelector(property) {
 }
 
 function toRow(obj) {
-  return mapKeys(obj, snakeCase);
+  return mapKeys(obj, (v, k) => snakeCase(k));
 }
 
 const todoColumns = Todo.getPropertyNames().map(toDbSelector);
@@ -40,14 +26,14 @@ class TodoResource {
   @GET('/v1/todos')
   async listTodos(req) {
     const rows = await this.query().select();
-    return json({
+    return {
       todos: rows.map(row => new Todo(row))
-    });
+    };
   }
 
   @POST('/v1/todos')
   async createTodo(req) {
-    const todo = new Todo(await readBody(req));
+    const todo = new Todo(await parsedBody(req));
     const [ insertId ] = await db('todos').insert(toRow(todo));
 
     return this.showTodo(req, { id: insertId });
@@ -56,18 +42,15 @@ class TodoResource {
   @GET('/v1/todos/:id')
   async showTodo(req, { id }) {
     const row = await this.query().first().where({ id });
-    return row && json(new Todo(row));
+    return row && new Todo(row);
   }
 
   @PUT('/v1/todos/:id')
   async updateTodo(req, { id }) {
-    const { label, done } = await readBody(req);
+    const changes = filter(pick(await parsedBody(req), 'label', 'done'));
 
-    if (label !== undefined || done !== undefined) {
-      const changes = { updated_at: new Date() };
-
-      if (label !== undefined) changes.label = label;
-      if (done !== undefined) changes.done = !!done;
+    if (!isEmpty(changes)) {
+      changes.updated_at = new Date();
 
       const changedRows = await db('todos').where({ id }).update(changes);
       if (changedRows === 0) return;
