cc77f67e3011863c0c19a62a4fa3bca94d6943c8
Jan Krems <jan.krems@groupon.com>
2015-06-11 22:52:54 -0700

    Use even an even more jax-rs-like cass

--- src/server.js
+++ src/server.js
@@ -1,35 +1,21 @@
 import { createServer } from 'http';
 
-import createApp from 'quinn';
-import createRouter from 'wegweiser';
+import quinn from 'quinn';
 import cass from 'cass';
-import Bluebird from 'bluebird';
-import { createGraph, Provides } from 'nilo';
+import { Provides } from 'nilo';
 
 import Store from './store';
 import Todo from './todos/model';
 import Todos from './todos/routes';
 
-const scope = createGraph({
+const app = cass(Todos);
+
+app.graph.scan({
   @Provides('todoStore')
   getTodoStore() { return new Store('todos', Todo); }
-}).createScope();
+});
 
-const router = createRouter(scope.construct(Todos));
-
-function errorLogger(handler) {
-  return async req => {
-    try {
-      return await Bluebird.resolve(handler(req));
-    } catch (err) {
-      console.error(err.stack);
-      return Bluebird.reject(err);
-    }
-  };
-}
-
-const app = createApp(cass(errorLogger(router)));
-const server = createServer(app);
+const server = createServer(quinn(app));
 
 server.listen(8000, function() {
   console.log('Listening.');
